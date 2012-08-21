c6124209da6bc4df565d5aae89eb55b87fc5847b
tomykaira <tomykaira@gmail.com>
2012-08-21 20:02:04 +0900

    Create top for impl

---
+++ top.vhd
@@ -0,0 +1,49 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+use IEEE.STD_LOGIC_UNSIGNED.all;
+
+entity top is
+  
+  port (
+    CLK, XRST, RS_RX       : in  std_logic;
+    RS_TX                  : out std_logic);
+
+end top;
+
+architecture top of top is
+
+  component mips
+    port (
+      clk, reset          : in  STD_LOGIC;
+      pc                  : out STD_LOGIC_VECTOR(31 downto 0);
+      instruction         : in  STD_LOGIC_VECTOR(31 downto 0);
+      mem_write           : out STD_LOGIC;
+      alu_out, write_data : out STD_LOGIC_VECTOR(31 downto 0);
+      read_data           : in  STD_LOGIC_VECTOR(31 downto 0));
+  end component;
+
+  component instruction_memory
+     port (
+       a  : in  std_logic_vector(5 downto 0);
+       rd : out std_logic_vector(31 downto 0));
+  end component;
+
+  component data_memory
+    port (
+      clk, we : in  std_logic;
+      a, wd   : in  std_logic_vector(31 downto 0);
+      rd      : out std_logic_vector(31 downto 0));
+  end component;
+
+  signal pc, instruction, read_data : std_logic_vector(31 downto 0);
+
+  signal write_data_buf, data_addr_buf : std_logic_vector(31 downto 0);
+  signal mem_write_buf : STD_LOGIC;
+
+begin  -- test
+
+  mips1 : mips port map(clk, not xrst, pc, instruction, mem_write_buf, data_addr_buf, write_data_buf, read_data);
+  imem1 : instruction_memory port map(pc(7 downto 2), instruction);
+  dmem1 : data_memory port map(clk, mem_write_buf, data_addr_buf, write_data_buf, read_data);
+
+end top;
