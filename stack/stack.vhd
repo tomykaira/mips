95435bcbc37250dbd0cfc6bdedc25fa27d70bff8
tomykaira <tomykaira@gmail.com>
2012-09-10 17:02:19 +0900

    Download stack implementation from http://vhdlguru.blogspot.jp/2011/01/implementation-of-stack-in-vhdl.html

---
+++ stack/stack.vhd
@@ -0,0 +1,78 @@
+-- from VHDL coding tips and tricks.  written by Vipin
+-- http://vhdlguru.blogspot.jp/2011/01/implementation-of-stack-in-vhdl.html
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity stack is
+port(   Clk : in std_logic;  --Clock for the stack.
+        Enable : in std_logic;  --Enable the stack. Otherwise neither push nor pop will happen.
+        Data_In : in std_logic_vector(15 downto 0);  --Data to be pushed to stack
+        Data_Out : out std_logic_vector(15 downto 0);  --Data popped from the stack.
+        PUSH_barPOP : in std_logic;  --active low for POP and active high for PUSH.
+        Stack_Full : out std_logic;  --Goes high when the stack is full.
+        Stack_Empty : out std_logic  --Goes high when the stack is empty.
+        );
+end stack;
+
+architecture Behavioral of stack is
+
+type mem_type is array (255 downto 0) of std_logic_vector(15 downto 0);
+signal stack_mem : mem_type := (others => (others => '0'));
+signal stack_ptr : integer := 255;
+signal full,empty : std_logic := '0';
+
+begin
+
+Stack_Full <= full;
+Stack_Empty <= empty;
+
+--PUSH and POP process for the stack.
+PUSH : process(Clk,PUSH_barPOP,Enable)
+begin
+    if(rising_edge(Clk)) then
+        --PUSH section.
+        if (Enable = '1' and PUSH_barPOP = '1' and full = '0') then
+             --Data pushed to the current address.
+            stack_mem(stack_ptr) <= Data_In;
+            if(stack_ptr /= 0) then
+                stack_ptr <= stack_ptr - 1;
+            end if;
+            --setting full and empty flags
+            if(stack_ptr = 0) then
+                full <= '1';
+                empty <= '0';
+            elsif(stack_ptr = 255) then
+                full <= '0';
+                empty <= '1';
+            else
+                full <= '0';
+                empty <= '0';
+            end if;
+           
+        end if;
+        --POP section.
+        if (Enable = '1' and PUSH_barPOP = '0' and empty = '0') then
+        --Data has to be taken from the next highest address(empty descending type stack).
+            if(stack_ptr /= 255) then  
+                Data_Out <= stack_mem(stack_ptr+1);
+                stack_ptr <= stack_ptr + 1;
+            end if;
+            --setting full and empty flags
+            if(stack_ptr = 0) then
+                full <= '1';
+                empty <= '0';
+            elsif(stack_ptr = 255) then
+                full <= '0';
+                empty <= '1';
+            else
+                full <= '0';
+                empty <= '0';
+            end if;
+           
+        end if;
+       
+    end if;
+end process;
+
+end Behavioral;
---
+++ stack/stack_tb.vhd
@@ -0,0 +1,74 @@
+-- from VHDL coding tips and tricks.  written by Vipin
+-- http://vhdlguru.blogspot.jp/2011/01/implementation-of-stack-in-vhdl.html
+
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.std_logic_arith.ALL;
+
+ENTITY stack_tb IS
+END stack_tb;
+
+ARCHITECTURE behavior OF stack_tb IS
+   --Inputs and outputs
+   signal Clk,Enable,PUSH_barPOP,Stack_Full,Stack_Empty : std_logic := '0';
+   signal Data_In,Data_Out : std_logic_vector(15 downto 0) := (others => '0');
+    --temporary signals
+    signal i : integer := 0;
+   -- Clock period definitions
+   constant Clk_period : time := 10 ns;
+
+BEGIN
+    -- Instantiate the Unit Under Test (UUT)
+   uut: entity work.stack PORT MAP (
+          Clk => Clk,
+          Enable => Enable,
+          Data_In => Data_In,
+          Data_Out => Data_Out,
+          PUSH_barPOP => PUSH_barPOP,
+          Stack_Full => Stack_Full,
+          Stack_Empty => Stack_Empty
+        );
+
+   -- Clock process definitions
+   Clk_process :process
+   begin
+        Clk <= '0';
+        wait for Clk_period/2;
+        Clk <= '1';
+        wait for Clk_period/2;
+   end process;
+
+   -- Stimulus process
+   stim_proc: process
+   begin       
+      wait for clk_period*3; --wait for 3 clock periods(simply)
+        Enable <= '1';  --Enable the stack.
+        PUSH_barPOP <= '1'; --Set for push operation.
+        for i in 0 to 255 loop  --Push integers from 0 to 255 to the stack.
+            Data_In <= conv_std_logic_vector(i,16);
+            wait for clk_period;
+        end loop;  
+        Enable <= '0';  --disable the stack.
+        wait for clk_period*2;
+        Enable <= '1'; --re-enable the stack.
+        PUSH_barPOP <= '0';  --Set for POP operation.
+        for i in 0 to 255 loop  --POP all elements from stack one by one.
+            wait for clk_period;
+        end loop;  
+        Enable <= '0'; --Disable stack.
+       
+        wait for clk_period*3;
+        Enable <= '1'; --Enable stack
+        PUSH_barPOP <= '1';  --Set for push operation.
+        Data_In <= X"FFFF";  --Push 65535 to stack.
+        wait for clk_period;
+        Data_In <= X"7FFF";  --Push 32767 to stack.
+        wait for clk_period;   
+        PUSH_barPOP <= '0';  --POP the above pushed values.
+        wait for clk_period*2;
+        Enable <= '0';     
+
+      wait;
+   end process;
+
+END;
