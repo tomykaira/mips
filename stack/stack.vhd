cb8c8ce238dcd33c82163eeba621594db3bbfebc
tomykaira <tomykaira@gmail.com>
2012-09-10 17:13:30 +0900

    Stack width and depth are configurable

--- stack/stack.vhd
+++ stack/stack.vhd
@@ -5,21 +5,25 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity stack is
-port(   Clk : in std_logic;  --Clock for the stack.
-        Enable : in std_logic;  --Enable the stack. Otherwise neither push nor pop will happen.
-        Data_In : in std_logic_vector(15 downto 0);  --Data to be pushed to stack
-        Data_Out : out std_logic_vector(15 downto 0);  --Data popped from the stack.
+  generic (
+    width : integer := 32;
+    depth : integer := 256
+    );
+port(   Clk         : in std_logic;  --Clock for the stack.
+        Enable      : in std_logic;  --Enable the stack. Otherwise neither push nor pop will happen.
+        Data_In     : in std_logic_vector(width-1 downto 0);  --Data to be pushed to stack
+        Data_Out    : out std_logic_vector(width-1 downto 0);  --Data popped from the stack.
         PUSH_barPOP : in std_logic;  --active low for POP and active high for PUSH.
-        Stack_Full : out std_logic;  --Goes high when the stack is full.
+        Stack_Full  : out std_logic;  --Goes high when the stack is full.
         Stack_Empty : out std_logic  --Goes high when the stack is empty.
         );
 end stack;
 
 architecture Behavioral of stack is
 
-type mem_type is array (255 downto 0) of std_logic_vector(15 downto 0);
+type mem_type is array (depth-1 downto 0) of std_logic_vector(width-1 downto 0);
 signal stack_mem : mem_type := (others => (others => '0'));
-signal stack_ptr : integer := 255;
+signal stack_ptr : integer := depth-1;
 signal full,empty : std_logic := '0';
 
 begin
@@ -42,19 +46,19 @@ begin
             if(stack_ptr = 0) then
                 full <= '1';
                 empty <= '0';
-            elsif(stack_ptr = 255) then
+            elsif(stack_ptr = depth-1) then
                 full <= '0';
                 empty <= '1';
             else
                 full <= '0';
                 empty <= '0';
             end if;
-           
+
         end if;
         --POP section.
         if (Enable = '1' and PUSH_barPOP = '0' and empty = '0') then
         --Data has to be taken from the next highest address(empty descending type stack).
-            if(stack_ptr /= 255) then  
+            if(stack_ptr /= depth-1) then
                 Data_Out <= stack_mem(stack_ptr+1);
                 stack_ptr <= stack_ptr + 1;
             end if;
@@ -62,16 +66,16 @@ begin
             if(stack_ptr = 0) then
                 full <= '1';
                 empty <= '0';
-            elsif(stack_ptr = 255) then
+            elsif(stack_ptr = depth-1) then
                 full <= '0';
                 empty <= '1';
             else
                 full <= '0';
                 empty <= '0';
             end if;
-           
+
         end if;
-       
+
     end if;
 end process;
 
--- stack/stack_tb.vhd
+++ stack/stack_tb.vhd
@@ -11,7 +11,7 @@ END stack_tb;
 ARCHITECTURE behavior OF stack_tb IS
    --Inputs and outputs
    signal Clk,Enable,PUSH_barPOP,Stack_Full,Stack_Empty : std_logic := '0';
-   signal Data_In,Data_Out : std_logic_vector(15 downto 0) := (others => '0');
+   signal Data_In,Data_Out : std_logic_vector(31 downto 0) := (others => '0');
     --temporary signals
     signal i : integer := 0;
    -- Clock period definitions
@@ -40,33 +40,38 @@ BEGIN
 
    -- Stimulus process
    stim_proc: process
-   begin       
+   begin
       wait for clk_period*3; --wait for 3 clock periods(simply)
         Enable <= '1';  --Enable the stack.
         PUSH_barPOP <= '1'; --Set for push operation.
         for i in 0 to 255 loop  --Push integers from 0 to 255 to the stack.
-            Data_In <= conv_std_logic_vector(i,16);
+            Data_In <= conv_std_logic_vector(i,32);
             wait for clk_period;
-        end loop;  
+        end loop;
         Enable <= '0';  --disable the stack.
         wait for clk_period*2;
+        assert Stack_Full = '1' report "255 items are pushed, but not full";
         Enable <= '1'; --re-enable the stack.
         PUSH_barPOP <= '0';  --Set for POP operation.
         for i in 0 to 255 loop  --POP all elements from stack one by one.
+            assert Data_Out = conv_std_logic_vector(255-i, 32) report "Data_Out is not equal to i";
             wait for clk_period;
-        end loop;  
+        end loop;
         Enable <= '0'; --Disable stack.
-       
+
         wait for clk_period*3;
         Enable <= '1'; --Enable stack
         PUSH_barPOP <= '1';  --Set for push operation.
-        Data_In <= X"FFFF";  --Push 65535 to stack.
+        Data_In <= X"FFFFFFFF";  --Push 65535 to stack.
         wait for clk_period;
-        Data_In <= X"7FFF";  --Push 32767 to stack.
-        wait for clk_period;   
+        Data_In <= X"7FFFFFFF";  --Push 32767 to stack.
+        wait for clk_period;
         PUSH_barPOP <= '0';  --POP the above pushed values.
-        wait for clk_period*2;
-        Enable <= '0';     
+        wait for clk_period;
+        assert Data_Out = x"7fffffff" report "Data_Out is not equal to 0x7fffffff";
+        wait for clk_period;
+        assert Data_Out = x"ffffffff" report "Data_Out is not equal to 0xffffffff";
+        Enable <= '0';
 
       wait;
    end process;
