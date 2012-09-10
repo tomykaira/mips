185ba5251db2b7c6d3039af9acb7121915d98f17
tomykaira <tomykaira@gmail.com>
2012-09-10 21:25:05 +0900

    insert program_counter into data_path

--- data_path.vhd
+++ data_path.vhd
@@ -41,24 +41,30 @@ architecture struct of data_path is
       op : in std_logic_vector(6 downto 0));
   end component;
 
-  signal write_reg_addr : std_logic_vector(4 downto 0);
-  signal pc_jump, pc_next, pc_next_branch, pc_plus4, pc_branch : std_logic_vector(31 downto 0);
-  signal sign_immediate, sign_immediate_sh : std_logic_vector(31 downto 0);
-  signal src_a, src_b, result : std_logic_vector(31 downto 0);
+  component program_counter is
+    port (
+      clk   : in STD_LOGIC;
+      reset : in std_logic;
+      pc    : out std_logic_vector(31 downto 0);
 
-  signal pc_buf, write_data_buf, alu_out_buf : std_logic_vector(31 downto 0) := (others => '0');
+      -- needed information
+      pc_src           : in std_logic_vector(2 downto 0);
+      jump             : in std_logic_vector(25 downto 0);
+      relative         : in std_logic_vector(31 downto 0); -- sign extended
+      reg              : in std_logic_vector(31 downto 0);
+      stack_top        : in std_logic_vector(31 downto 0);
+      branch_condition : in STD_LOGIC
+    );
+
+  end component;
+
+  signal write_reg_addr : std_logic_vector(4 downto 0);
+  signal sign_immediate : std_logic_vector(31 downto 0);
+  signal read_data1, src_b, result : std_logic_vector(31 downto 0);
+
+  signal pc_buf, read_data2, alu_out_buf : std_logic_vector(31 downto 0) := (others => '0');
 
 begin  -- struct
-
-  pc_jump <= pc_plus4(31 downto 28) & instruction(25 downto 0) & "00" when alu_src = '1' else
-             write_data_buf;
-
-  pc_plus4 <= pc_buf + 4;
-  sign_immediate_sh <= sign_immediate(29 downto 0) & "00";
-  pc_branch <= pc_plus4 + sign_immediate_sh;
-
-  pc_next_branch <= pc_branch when pc_src = '1' else pc_plus4;
-  pc_next <= pc_jump when jump = '1' else pc_next_branch;
 
   rf : register_selector port map (
     clk           => clk,
@@ -68,8 +74,27 @@ begin  -- struct
     read_addr2    => instruction(20 downto 16),
     write_addr3   => write_reg_addr,
     write_data3   => result,
-    read_data1    => src_a,
-    read_data2    => write_data_buf);
+    read_data1    => read_data1,
+    read_data2    => read_data2);
+
+  main_alu : alu port map (
+    a       => read_data1,
+    b       => src_b,
+    control => alu_control,
+    output  => alu_out_buf,
+    zero    => zero);
+
+  pc_manager : program_counter port map (
+    clk              => clk,
+    reset            => reset,
+    pc               => pc,
+    pc_src           => pc_src,
+    jump             => instruction(25 downto 0),
+    relative         => sign_immediate,
+    reg              => read_data1,
+    stack_top        => stack_top,   -- TODO
+    branch_condition => branch_condition); -- TODO
+
 
   write_reg_addr <= instruction(15 downto 11) when reg_dst = '1' else instruction(20 downto 16);
   result <= data_from_bus when bus_to_reg = '1' else alu_out_buf;
@@ -77,18 +102,10 @@ begin  -- struct
   sign_immediate <= x"ffff" & instruction(15 downto 0) when instruction(15) = '1'
                     else x"0000" & instruction(15 downto 0);
 
-  src_b <= sign_immediate when alu_src = '1' else write_data_buf;
-
-  main_alu : alu port map (
-    a       => src_a,
-    b       => src_b,
-    control => alu_control,
-    output  => alu_out_buf,
-    zero    => zero);
+  src_b <= sign_immediate when alu_src = '1' else read_data2;
 
 
   alu_out    <= alu_out_buf;
-  pc         <= pc_buf;
-  write_data <= pc_buf + 8 when write_pc = '1' else write_data_buf;
+  write_data <= read_data2;
   
 end struct;
