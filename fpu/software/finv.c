929e6149f3b349d34fef90da039eb991f0a92c37
tomykaira <tomykaira@gmail.com>
2012-09-11 18:09:58 +0900

    Trying finv, but not works

--- fpu/software/finv.c
+++ fpu/software/finv.c
@@ -9,21 +9,44 @@
 #define DOTS 0
 #define TESTCASE 1
 #define D(x) { if (DEBUG) { x ; } }
+#define MANTISSA(x) (0x800000 + (x & 0x7fffff))
+#define EXP(x) ((x & 0x7f800000) >> 23)
 
 union IntAndFloat {
     unsigned int ival;
     float fval;
 };
 
+unsigned int x0(unsigned int key)
+{
+  union IntAndFloat v1, v2;
+  // 2^n has nothing to do with mantissa
+  v1.fval = 1/((float) key);
+  v2.fval = 1/(((float) key + 1));
+  return EXP(v1.ival) != EXP(v2.ival) ? MANTISSA(v1.ival)/2 + MANTISSA(v2.ival) : MANTISSA(v1.ival)/2 + MANTISSA(v2.ival)/2;
+}
+unsigned int finv_table_const(unsigned int key)
+{
+  unsigned int x = x0(key);
+  printf("%d %x\n", x, x);
+  return ((x << 2) - ((key*x*x) >> 33));
+}
+
+unsigned int finv_table_inc(unsigned int key)
+{
+  unsigned int x = x0(key);
+  return (x*x) >> 46;
+}
+
 unsigned int finv(unsigned int a)
 {
-  unsigned int x, man;
-  float x;
-  man = 0x800000 + (a & 0x7fffff);
-  x = 1/((man >> 13)*1024.0*2.0) + 1/(((man >> 13) + 1)*1024.0*2.0);
-  printf("%f", x);
-  printf("%f", 2*x + man*x*x);
-  return 0;
+  unsigned long c0;
+  unsigned long diff, key;
+  key = (a >> 13) & 0x3ff;
+  printf("const: %d %x, inc: %d %x\n", finv_table_const(key), finv_table_const(key), finv_table_inc(key), finv_table_inc(key));
+  diff = finv_table_inc(key)*(a&0x1fff);
+  c0 = finv_table_const(key) - ((diff >> 12) & 0x1fff);
+  return (a&0x80000000) + (((key == 0 && diff == 0 ? 254 : 253) - EXP(a)) << 23) + (c0 & 0x7fffff);
 }
 
 void print_float(unsigned int x) {
@@ -57,13 +80,13 @@ void test(unsigned int a) {
 
   // generate testcase for verilog
   if (TESTCASE) {
-    printf("%08x\n%08x\n%08x\n", a, res.ival);
+    printf("%08x\n%08x\n", a, res.ival);
   }
 
   if (!DEBUG && abs(res.ival - res2.ival) < 4) {
     if (DOTS) {printf(".");}
   } else {
-    printf("a: %x, b: %x\n", a, b);
+    printf("a: %x\n", a);
     printf("  expected: ");
     print_float(res2.ival);
     printf("\n");
@@ -71,17 +94,18 @@ void test(unsigned int a) {
     printf("    actual: ");
     print_float(res.ival);
     printf("\n");
-    exit(1);
+    // exit(1);
   }
 }
 
 // test: 負数
 int main(int argc, char *argv[])
 {
-  unsigned int a, b, s;
   long long i;
-  for (i = 0;i < 10; i++) {
-    test(rand_float());
+  union IntAndFloat input;
+
+  while (scanf("%f", &input.fval) != EOF) {
+    test(input.ival);
   }
 
   return 0;
