db378eb2fc8d81e740901edfc9cbd485ffdc2bb7
tomykaira <tomykaira@gmail.com>
2012-09-11 11:13:53 +0900

    whitespace

--- data_path.vhd
+++ data_path.vhd
@@ -4,11 +4,11 @@ use IEEE.STD_LOGIC_ARITH.all;
 use IEEE.STD_LOGIC_UNSIGNED.all;
 
 entity data_path is
-  
+
   port (
     clk, reset           : in  std_logic;
     bus_to_reg           : in  std_logic;
-		pc_src               : in  std_logic_vector(2 downto 0);
+    pc_src               : in  std_logic_vector(2 downto 0);
     alu_src, reg_dst     : in  std_logic;
     reg_write            : in  std_logic;
     alu_control          : in  std_logic_vector(3 downto 0);
@@ -16,7 +16,7 @@ entity data_path is
     data_from_bus        : in  std_logic_vector(31 downto 0);
     pc                   : out std_logic_vector(31 downto 0);
     mem_addr, write_data : out std_logic_vector(31 downto 0)
-    ); 
+    );
 end data_path;
 
 architecture struct of data_path is
@@ -57,40 +57,40 @@ architecture struct of data_path is
 
   end component;
 
-	component branch_condition_checker is
-		
-		port (
-			op        : in std_logic_vector(5 downto 0);
-			a, b      : in std_logic_vector(31 downto 0);
-			go_branch : out STD_LOGIC
-			);
+  component branch_condition_checker is
 
-	end component;
+    port (
+      op        : in std_logic_vector(5 downto 0);
+      a, b      : in std_logic_vector(31 downto 0);
+      go_branch : out STD_LOGIC
+      );
 
-	component call_stack is
-  
-		port (
-			clk        : in STD_LOGIC;
-			-- decide to push
-			op         : in std_logic_vector(5 downto 0);
-			-- as input data
-			current_pc : in std_logic_vector(31 downto 0);
-			pc_src     : in std_logic_vector(2 downto 0);
+  end component;
 
-			stack_top  : out std_logic_vector(31 downto 0)
-			);
+  component call_stack is
 
-	end component;
+    port (
+      clk        : in STD_LOGIC;
+      -- decide to push
+      op         : in std_logic_vector(5 downto 0);
+      -- as input data
+      current_pc : in std_logic_vector(31 downto 0);
+      pc_src     : in std_logic_vector(2 downto 0);
+
+      stack_top  : out std_logic_vector(31 downto 0)
+      );
+
+  end component;
 
   signal write_reg_addr : std_logic_vector(4 downto 0);
   signal sign_immediate : std_logic_vector(31 downto 0);
   signal read_data1, read_data2, src_b, result, alu_out_buf : std_logic_vector(31 downto 0) := (others => '0');
 
-	signal op : std_logic_vector(5 downto 0);
+  signal op : std_logic_vector(5 downto 0);
 
-	signal stack_top, pc_buf : std_logic_vector(31 downto 0);
+  signal stack_top, pc_buf : std_logic_vector(31 downto 0);
 
-	signal branch_condition : STD_LOGIC;
+  signal branch_condition : STD_LOGIC;
 
 begin  -- struct
 
@@ -123,23 +123,23 @@ begin  -- struct
     stack_top        => stack_top,
     branch_condition => branch_condition);
 
-	-- comparation inputs are always from register
-	branch_condition_checker_inst : branch_condition_checker port map (
-		op => op,
-		a => read_data1,
-		b => read_data2,
-		go_branch => branch_condition
-		);
+  -- comparation inputs are always from register
+  branch_condition_checker_inst : branch_condition_checker port map (
+    op => op,
+    a => read_data1,
+    b => read_data2,
+    go_branch => branch_condition
+    );
 
-	call_stack_inst : call_stack port map (
-		clk        => clk,
-		op         => op,
-		current_pc => pc_buf,
-		pc_src     => pc_src,
-		stack_top  => stack_top
-		);
+  call_stack_inst : call_stack port map (
+    clk        => clk,
+    op         => op,
+    current_pc => pc_buf,
+    pc_src     => pc_src,
+    stack_top  => stack_top
+    );
 
-	op <= instruction(31 downto 26); -- alias
+  op <= instruction(31 downto 26); -- alias
 
   write_reg_addr <= instruction(15 downto 11) when reg_dst = '1' else instruction(20 downto 16);
   result <= data_from_bus when bus_to_reg = '1' else alu_out_buf;
@@ -152,6 +152,6 @@ begin  -- struct
 
   mem_addr   <= alu_out_buf;
   write_data <= read_data2;
-	pc <= pc_buf;
-  
+  pc <= pc_buf;
+
 end struct;
