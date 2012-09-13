27342b285c24df2d0b0b251b54b2ca2e2e8b457f
tomykaira <tomykaira@gmail.com>
2012-09-13 13:12:47 +0900

    Change MEM_SIZE expression for sed

--- instruction_memory.v
+++ instruction_memory.v
@@ -5,7 +5,7 @@ module instruction_memory (input clk,
                            input [15:0]  a,
                            output reg [31:0] rd);
    // size is fixed to 64KB = 2^16
-   parameter MEM_SIZE = 64*1024;
+   parameter MEM_SIZE=1450;
 
    reg [31:0] RAM[MEM_SIZE-1:0];
 
