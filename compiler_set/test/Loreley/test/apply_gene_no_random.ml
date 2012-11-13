8a8ea9351dafab27f88dcbe343c1806893e7d2af
tomykaira <tomykaira@gmail.com>
2012-11-13 23:16:56 +0900

    Add apply_gene test and its testcases

---
+++ compiler_set/test/Loreley/test/apply_gene_no_random.ml
@@ -0,0 +1,456 @@
+(* functions including random are removed, because they are difficult to compare *)
+(*INPUT
+21 0
+
+0 0.5 0.5 5
+-0.0557357 0.296215 0.345357 0.819319 0.740205 -0.591994
+-0.212083 0.653373 -0.458351 0.589448 -0.535298 0.526096
+1 2 0.349591
+1 3 0.286532
+1 4 0.0100913
+3 5 0.0804772 -0.265128 -0.613688
+1 6 0.273309
+
+1 0.5 0.5 5
+-0.848127 0.670267 0.618477 0.456033 -0.112178 0.36084
+-0.599703 -0.815963 -0.293803 -0.780384 0.924242 0.114203
+1 2 0.349591
+1 3 0.286532
+1 4 0.0100913
+3 5 0.0804772 0.0112989 0.866422
+1 6 0.273309
+
+1 0 1 1
+-1 0 0 1 0 0
+1 0 0 1 0 0
+1 1 1
+
+0.235294 0 1 1
+-0.082579 0.996584 -0.996584 -0.082579 0 0
+1 0 0 1 0 0
+1 1 1
+
+0.764706 0 1 1
+-0.082579 -0.996584 0.996584 -0.082579 0 0
+1 0 0 1 0 0
+1 1 1
+
+0.117647 0 1 1
+0.546948 0.837166 -0.837166 0.546948 0 0
+1 0 0 1 0 0
+1 1 1
+
+0.882353 0 1 1
+0.546948 -0.837166 0.837166 0.546948 0 0
+1 0 0 1 0 0
+1 1 1
+
+0.294118 0 1 1
+-0.401695 0.915773 -0.915773 -0.401695 0 0
+1 0 0 1 0 0
+1 1 1
+
+0.705882 0 1 1
+-0.401695 -0.915773 0.915773 -0.401695 0 0
+1 0 0 1 0 0
+1 1 1
+
+0 0 1 1
+0.945817 0.324699 -0.324699 0.945817 0 0
+1 0 0 1 0 0
+1 1 1
+
+1 0 1 1
+0.945817 -0.324699 0.324699 0.945817 0 0
+1 0 0 1 0 0
+1 1 1
+
+0.176471 0 1 1
+0.245485 0.9694 -0.9694 0.245485 0 0
+1 0 0 1 0 0
+1 1 1
+
+0.823529 0 1 1
+0.245485 -0.9694 0.9694 0.245485 0 0
+1 0 0 1 0 0
+1 1 1
+
+0.470588 0 1 1
+-0.986361 0.164595 -0.164595 -0.986361 0 0
+1 0 0 1 0 0
+1 1 1
+
+0.529412 0 1 1
+-0.986361 -0.164595 0.164595 -0.986361 0 0
+1 0 0 1 0 0
+1 1 1
+
+0.411765 0 1 1
+-0.879474 0.475947 -0.475947 -0.879474 0 0
+1 0 0 1 0 0
+1 1 1
+
+0.588235 0 1 1
+-0.879474 -0.475947 0.475947 -0.879474 0 0
+1 0 0 1 0 0
+1 1 1
+
+0.352941 0 1 1
+-0.677282 0.735724 -0.735724 -0.677282 0 0
+1 0 0 1 0 0
+1 1 1
+
+0.647059 0 1 1
+-0.677282 -0.735724 0.735724 -0.677282 0 0
+1 0 0 1 0 0
+1 1 1
+
+0.0588235 0 1 1
+0.789141 0.614213 -0.614213 0.789141 0 0
+1 0 0 1 0 0
+1 1 1
+
+0.941176 0 1 1
+0.789141 -0.614213 0.614213 0.789141 0 0
+1 0 0 1 0 0
+1 1 1
+*)
+let dnaArray          = ref (Array.create 0 (0., 0., 0., (0., 0., 0., 0., 0., 0.), [], (0., 0., 0., 0., 0., 0.))) in
+let genes             = ref 0 in
+let rec gene_gen res=
+  if res==0 then []
+  else 
+  (
+   let head=read_float () in
+   head::(gene_gen (res - 1))
+  )
+in
+
+let rec dna_gen res=
+  if res==0 then []
+  else
+  (
+  let len=read_int () in
+  let fType=read_int () in
+  let head=(fType,gene_gen len) in
+  head::(dna_gen (res - 1))
+  )
+in
+
+let read_dnas n=
+let cIndex=read_float () in
+let cBlendRate=read_float () in
+let cWeight=read_float () in
+let num_dna=read_int () in
+let c1=read_float () in
+let c2=read_float () in
+let c3=read_float () in
+let c4=read_float () in
+let c5=read_float () in
+let c6=read_float () in
+let d1=read_float () in
+let d2=read_float () in
+let d3=read_float () in
+let d4=read_float () in
+let d5=read_float () in
+let d6=read_float () in
+(cIndex,cBlendRate,cWeight,(c1,c2,c3,c4,c5,c6),dna_gen num_dna,(d1,d2,d3,d4,d5,d6))
+in
+
+let read_gene ()=
+(genes := read_int();let _ = read_int() in
+dnaArray:=Array.init (!genes) read_dnas)
+in
+let apply_func fn_coeff x y=
+  let (fn,coeffs) = fn_coeff in
+(
+ match fn with
+ 1 -> (
+   (* linear *)
+   let weight::[]=coeffs in
+   (x*.weight,y*.weight)
+  )
+ |2 ->
+  (
+   (* sinusoid *)
+   let weight::[]=coeffs in
+   ((sin x)*.weight,(sin y)*.weight)
+  )
+ |3 ->
+  (
+   (* fisheye *)
+   let weight::[]=coeffs in
+   let tmp=sqrt (x*.x+.y*.y) in
+   let r=2.0*.weight /. (tmp+.1.0) in
+   (r*.y,r*.x)
+  )
+ |4 ->
+  (
+   (* exponential *)
+   let weight::[]=coeffs in
+   let dx=weight*.(exp (x-.1.0)) in
+   let dy=y*.3.141592653 in
+   let nx=(cos dy)*.dx in
+   let ny=(sin dy)*.dx in
+   (nx,ny)
+  )
+ |5 ->
+  (
+   (* bent2 *)
+   let weight::bx::by::[]=coeffs in
+   let nx=if x<0.0 then x*.bx else x in
+   let ny=if y<0.0 then y*.by else y in
+   (weight*.nx,weight*.ny)
+  )
+ |6 ->
+  (
+   (* sec *)
+   let weight::[]=coeffs in
+   let sinx=sin x in
+   let cosx=cos x in
+   let sinhy=sinh y in
+   let coshy=cosh y in
+   let v=2.0 /. ((cos (2.0*.x)) +. (cosh (2.0*.y))) in
+   (weight*.v*.cosx*.coshy,weight*.v*.sinx*.sinhy)
+  )
+ |10 ->
+  (
+   (* perspective *)
+   let weight::angle::dist::[]=coeffs in
+   let ang=angle*.3.14159265*.0.5 in
+   let vsin=sin ang in
+   let vfcos=dist*.(cos ang) in
+   let t=1.0/.(dist-.y*.vsin) in
+   (weight*.dist*.x*.t,weight*.vfcos*.y*.t)
+  )
+ |11 ->
+  (
+   (* spherical *)
+    let weight::[]=coeffs in
+    let r2=weight/.(x*.x+.y*.y+. 1e-10) in
+    (r2*.x,r2*.y)
+  )
+ |12 ->
+  (
+   (* swirl *)
+    let weight::[]=coeffs in
+    let r2=x*.x+.y*.y in
+    let c1,c2=sin r2,cos r2 in
+    let nx,ny=c1*.x-.c2*.y , c2*.x+.c1*.y in
+    (weight*.nx,weight*.ny)
+  )
+ |13 ->
+  (
+   (*eyefish*)
+    let weight::[]=coeffs in
+    let r=(weight*.2.0)/.(sqrt ((x*.x+.y*.y) +. 1.0)) in
+    (r*.x,r*.y)
+  )
+ |16 ->
+ (
+  (* fn16:disc *)
+  let weight::[]=coeffs in
+  let a=(atan2 x y)*.0.31830988618 in
+  let r=3.14159265*.(sqrt ((x*.x)+.(y*.y))) in
+  let sr,cr=sin r,cos r in
+  (weight*.sr*.a,weight*.cr*.a)
+ )
+ |18 ->
+ (
+  (* fn18:curl/curl_c1/curl_c2 *)
+   let weight::c1::c2::[]=coeffs in
+   let re=1.0+.c1*.x+.c2*.(x*.x-.y*.y) in
+   let im=c1*.y+.2.0*.c2*.x*.y in
+   let r=weight/.(re*.re+.im*.im) in
+   ((x*.re+.y*.im)*.r,(y*.re-.x*.im)*.r)
+ )
+ |19 ->
+ (
+  (* fn19:rectangles/rectangles_x/rectangles_y *)
+   let weight::rect_x::rect_y::[]=coeffs in
+   ((if rect_x==0.0 then weight*.x else weight*.((2.0*.(floor (x/.rect_x))+.1.0)*.rect_x-.x)),
+    (if rect_y==0.0 then weight*.y else weight*.((2.0*.(floor (y/.rect_y))+.1.0)*.rect_y-.y)))
+ )
+ |21 ->
+ (
+   (* fn21:waves/c0/c1/waves_dx2/waves_dy2 *)
+   let weight::c0::c1::dx2::dy2::[]=coeffs in
+   let nx,ny=x+.c0*.(sin (y*.dx2)),y+.c1*.(sin (x*.dy2)) in
+   (weight*.nx,weight*.ny)
+ )
+ |25 ->
+ (
+  (*25 fan2::x::y*)
+  let weight::fanX::fanY::[]=coeffs in
+  let dx=3.14159265*.(x*.x+.(1e-10)) in
+  let dx2=0.5*.dx in
+  let a=atan2 x y in
+  let r=weight*.(sqrt (x*.x+.y*.y)) in
+  let t=a+.y-.dx*.(floor (((a+.y)/.dx)+.0.5)) in
+  let a2=if t>dx2 then a-.dx2 else a+.dx2 in
+  let sa=sin a2 in
+  let ca=cos a2 in
+  (r*.sa,r*.ca)
+ )
+ |26 ->
+ (
+  (*26 disc2::rot::twist *)
+  let weight::rot::twist::timespi::cosadd::sinadd::[]=coeffs in
+  let t=timespi*.(x+.y) in
+  let sinr=sin t in
+  let cosr=cos t in
+  let r=weight*.(atan2 x y)/.3.14159265 in
+  ((sinr+.cosadd)*.r,(cosr+.sinadd)*.r)
+ )
+ |28 ->
+ (
+  (*28 rings*)
+  let fmod p q=
+    let r=(p/.q) in
+    let s=if r>=0.0 then 1.0 else (-1.0) in
+    let r_af=floor (abs_float r) in
+    p-.(q*.r_af)*.s
+  in
+  let weight::c::[]=coeffs in
+  let dx = c*.c+.(1e-10) in
+  let r = sqrt ((x*.x)+.(y*.y)) in
+  let a=atan2 x y in
+  let r2 = weight*.(fmod (r+.dx) (2.0*.dx)-.dx+.r*.(1.0-.dx))in
+  let cosa,sina=(cos a)*.r2,(sin a)*.r2 in
+  (weight*.cosa,weight*.sina)
+ )
+ |29 ->
+ (
+  (*29 polar *)
+  let weight::[]=coeffs in
+  let nx=(atan2 x y)*.0.318309886 in
+  let ny=(sqrt (x*.x+.y*.y))-.1.0 in
+  (weight*.nx,weight*.ny)
+ )
+ |_ ->
+   (0.0,0.0)
+)
+in
+
+let rec apply_gene_sub fn_ary x y retx rety=
+  if fn_ary==[] then (retx,rety)
+  else
+    (
+     let head::tail=fn_ary in
+       (let (nx,ny)=apply_func head x y in
+       apply_gene_sub tail x y (retx+.nx) (rety+.ny))
+    )
+in
+
+let apply_gene fn x y col=
+  let (cIndex,cBlendRate,cWeight,(c1,c2,c3,c4,c5,c6),fun_ary,(d1,d2,d3,d4,d5,d6))= !dnaArray.(fn) in
+  let nx,ny=apply_gene_sub fun_ary (c1*.x+.c3*.y+.c5) (c2*.x+.c4*.y+.c6) 0.0 0.0 in
+  ((d1*.nx+.d3*.ny+.d5),(d2*.nx+.d4*.ny+.d6),(col *. (1.0 -. cBlendRate)) +. cIndex*.cBlendRate)
+in
+read_gene();
+let test fn x y c =
+  let (x, y, c) = apply_gene fn x y c in
+  print_int5 x;
+  print_int5 y;
+  print_int5 c
+in
+test 4 (-0.448858) (0.266176) (0.127633);
+test 9 (0.302333) (0.425344) (0.127633);
+test 12 (0.147843) (0.500465) (0.127633);
+test 2 (0.521444) (-0.020463) (0.127633);
+test 5 (-0.521444) (-0.020463) (0.127633);
+test 7 (-0.268072) (-0.447727) (0.127633);
+test 13 (0.517700) (-0.065644) (0.127633);
+test 12 (-0.499834) (0.149959) (0.127633);
+test 14 (0.022668) (0.521352) (0.127634);
+test 6 (0.063453) (-0.517972) (0.127634);
+test 10 (-0.398924) (-0.336424) (0.127634);
+test 12 (-0.486545) (-0.188666) (0.127634);
+test 5 (-0.302332) (0.425343) (0.127634);
+test 1 (-0.521442) (-0.020462) (0.127634);
+test 6 (0.628366) (-0.341277) (0.563817);
+test 4 (0.057978) (-0.712708) (0.563817);
+test 3 (-0.715061) (0.001074) (0.563817);
+test 3 (0.057978) (-0.712707) (0.563817);
+test 6 (0.705485) (0.116635) (0.563817);
+test 8 (0.483506) (-0.526815) (0.563817);
+test 7 (-0.676665) (-0.231163) (0.563817);
+test 19 (0.483506) (-0.526814) (0.563817);
+test 2 (0.705130) (-0.118755) (0.563817);
+test 9 (-0.705131) (-0.118755) (0.563817);
+test 1 (-0.628365) (-0.341276) (0.563817);
+test 1 (0.814456) (-0.018640) (0.781909);
+test 12 (0.894057) (0.051581) (0.890954);
+test 19 (0.269480) (-0.854036) (0.890954);
+test 11 (0.737218) (-0.508437) (0.890955);
+test 7 (0.673855) (0.589845) (0.890955);
+test 7 (-0.810849) (0.380160) (0.890955);
+test 19 (-0.022427) (-0.895262) (0.890955);
+test 18 (0.532184) (-0.720263) (0.890955);
+test 0 (-0.890353) (0.096281) (0.890955);
+test 16 (-0.539866) (0.580893) (0.445478);
+test 20 (0.751273) (-0.253933) (0.445478);
+test 13 (0.436891) (-0.661830) (0.445478);
+test 13 (-0.321998) (0.724714) (0.445478);
+test 5 (0.198322) (-0.767829) (0.445478);
+test 18 (0.751272) (-0.253934) (0.445478);
+test 7 (-0.695648) (-0.380744) (0.445478);
+test 3 (0.628114) (-0.484113) (0.445478);
+test 10 (0.430590) (0.665946) (0.445478);
+test 15 (0.623492) (0.490051) (0.445478);
+test 13 (-0.781583) (-0.134238) (0.445478);
+test 9 (0.793018) (0.003762) (0.445478);
+test 19 (0.748828) (0.261051) (0.445478);
+test 5 (0.430590) (0.665946) (0.445478);
+test 7 (-0.321997) (0.724713) (0.445478);
+test 13 (-0.534328) (-0.585990) (0.445478);
+test 6 (0.623492) (0.490050) (0.445478);
+test 20 (0.751270) (-0.253935) (0.445478);
+test 10 (0.436888) (-0.661830) (0.445478);
+test 5 (0.198321) (-0.767828) (0.445478);
+test 17 (0.751270) (-0.253934) (0.445478);
+test 14 (-0.321996) (0.724713) (0.445478);
+test 5 (0.436889) (-0.661830) (0.445478);
+test 14 (0.793017) (0.003762) (0.445478);
+test 19 (-0.781582) (-0.134237) (0.445478);
+test 15 (-0.534328) (-0.585990) (0.445478);
+test 8 (0.748828) (0.261051) (0.445478);
+test 12 (-0.061737) (-0.790619) (0.445478);
+test 3 (-0.781581) (-0.134238) (0.445478);
+test 7 (0.198321) (-0.767826) (0.445478);
+test 9 (0.623490) (0.490049) (0.445478);
+test 9 (0.430589) (0.665943) (0.445478);
+test 20 (0.191027) (0.769672) (0.445478);
+test 14 (0.623490) (0.490049) (0.445479);
+test 12 (-0.534327) (-0.585988) (0.445479);
+test 19 (-0.699226) (0.374125) (0.445479);
+test 10 (-0.781581) (-0.134236) (0.445479);
+test 13 (-0.782819) (0.126815) (0.445479);
+test 20 (0.751269) (-0.253934) (0.445479);
+test 19 (0.436887) (-0.661829) (0.445479);
+test 20 (0.751270) (-0.253934) (0.445479);
+test 3 (0.436888) (-0.661829) (0.445479);
+test 5 (0.623491) (0.490049) (0.445479);
+test 4 (-0.069235) (0.789997) (0.445479);
+test 17 (0.793015) (0.003761) (0.445479);
+test 4 (-0.539862) (0.580893) (0.445479);
+test 11 (0.623490) (0.490049) (0.445479);
+test 20 (-0.321996) (0.724711) (0.445479);
+test 2 (0.191027) (0.769673) (0.445479);
+test 16 (-0.191027) (0.769673) (0.445479);
+test 7 (0.534327) (-0.585989) (0.445479);
+test 3 (0.321996) (0.724711) (0.445479);
+test 20 (-0.748825) (0.261051) (0.445479);
+test 7 (-0.430588) (0.665944) (0.445479);
+test 20 (-0.436889) (-0.661827) (0.445479);
+test 13 (-0.751270) (-0.253933) (0.445479);
+test 1 (0.782819) (0.126814) (0.445479);
+test 12 (0.849963) (-0.028671) (0.722740);
+test 3 (0.180859) (-0.830992) (0.722740);
+test 13 (0.813218) (0.248864) (0.722740);
+test 3 (-0.843088) (-0.111618) (0.722740);
+test 9 (0.180858) (-0.830991) (0.722740);
+test 15 (0.440881) (-0.727241) (0.722740);
+test 19 (-0.041615) (0.849425) (0.722740);
+test 4 (-0.554568) (0.644756) (0.722740);
+test 11 (0.688349) (0.499431) (0.722740);
