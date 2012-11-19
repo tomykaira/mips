f152b97709e905fd13f703e55c62b433f1b1ed40
tomykaira <tomykaira@gmail.com>
2012-11-20 08:49:49 +0900

    Add sign extension module

---
+++ 2nd/sign_extension/sign_extension.v
@@ -0,0 +1,10 @@
+// clock edged sign extension
+module sign_extension (input clk,
+                       input [15:0]  imm_in,
+                       output [31:0] imm_out);
+
+   always @ (posedge(clk)) begin
+      imm_out[31:15] <= {17{imm_in[15]}};
+      imm_out[14:0] <= imm_in[14:0];
+   end
+endmodule
