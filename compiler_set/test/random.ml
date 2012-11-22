d400d3719397ec9f6d85a6cd53f9aab230741220
tomykaira <tomykaira@gmail.com>
2012-11-23 01:53:50 +0900

    テストファイルの整理

--- compiler_set/test/random.ml
+++ compiler_set/test/random.ml
@@ -1,8 +1,9 @@
+(*NOMINCAML let xor = (lxor) in *)
 let random_source =
   let rs = Array.create 4 0 in
-  rs.(0) <- 123456789; 
+  rs.(0) <- 123456789;
   rs.(1) <- 362436069;
-  rs.(2) <- 521288629; 
+  rs.(2) <- 521288629;
   rs.(3) <- 88675123;
   rs
 in
@@ -20,10 +21,10 @@ let rec random_int max=
   ((mod) ((land) (xor128 ()) (((lsl) 1 31) - 1)) max)
 in
 
-print_int (mod 5 2);
-print_int (mod 5 (-2));
-print_int (mod (-5) 2);
-print_int (mod (-5) (-2));
+print_int ((mod) 5 2);
+print_int ((mod) 5 (-2));
+print_int ((mod) (-5) 2);
+print_int ((mod) (-5) (-2));
 print_int ((land) 9 5);
 print_int (((lsl) 1 15) - 1);
 print_int (random_int 10000)
