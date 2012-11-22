afbea696b952a958389431f3cf38de48fd45d9a2
tomykaira <tomykaira@gmail.com>
2012-11-22 18:12:39 +0900

    LDR, FLDR で RT を使わない扱いしていた間違いを解消

--- 2nd/decoder/decoder.v
+++ 2nd/decoder/decoder.v
@@ -12,18 +12,49 @@ module decoder(input clk,
                 output reg [15:0] raw_imm,
                 output            keep_pc);
 
-   parameter LDI  = 6'b101000;
-   parameter STI  = 6'b101001;
-   parameter LDR  = 6'b101100;
-   parameter FLDI = 6'b101010;
-   parameter FSTI = 6'b101011;
-   parameter FLDR = 6'b101110;
+   parameter ADD     = 6'b000000;
+   parameter SUB     = 6'b000001;
+   parameter XOR     = 6'b000010;
+   parameter ADDI    = 6'b000011;
+   parameter SUBI    = 6'b000100;
+   parameter XORI    = 6'b000101;
+   parameter SLLI    = 6'b000110;
+   parameter SRAI    = 6'b000111;
 
-   parameter JR     = 6'b111001;
-   parameter CALLR  = 6'b111011;
-   parameter INPUTB = 6'b111101;
+   parameter FMVLO   = 6'b010010;
+   parameter FMVHI   = 6'b010011;
+   parameter IMOVF   = 6'b010110;
+   parameter FMOVI   = 6'b010111;
 
-   parameter HALT   = 6'b111111;
+   parameter FADD    = 6'b110000;
+   parameter FSUB    = 6'b110001;
+   parameter FMUL    = 6'b110010;
+   parameter FMULN   = 6'b110011;
+   parameter FINV    = 6'b110100;
+   parameter FSQRT   = 6'b110101;
+
+   parameter LDI     = 6'b101000;
+   parameter LDR     = 6'b101100;
+   parameter STI     = 6'b101001;
+   parameter FLDI    = 6'b101010;
+   parameter FSTI    = 6'b101011;
+   parameter FLDR    = 6'b101110;
+
+   parameter BEQ     = 6'b100000;
+   parameter BLT     = 6'b100001;
+   parameter BLE     = 6'b100010;
+   parameter FBEQ    = 6'b100100;
+   parameter FBLT    = 6'b100101;
+   parameter FBLE    = 6'b100110;
+
+   parameter J       = 6'b111000;
+   parameter JR      = 6'b111001;
+   parameter CALL    = 6'b111010;
+   parameter CALLR   = 6'b111011;
+   parameter RETURN  = 6'b111100;
+   parameter INPUTB  = 6'b111101;
+   parameter OUTPUTB = 6'b111110;
+   parameter HALT    = 6'b111111;
 
    wire [5:0] op;
    assign op = inst[31:26];
@@ -96,29 +127,27 @@ module decoder(input clk,
       else
         use_rs <= 1'b1;
 
-      if (op == 6'b000011
-          || op == 6'b000100
-          || op == 6'b000101
-          || op == 6'b000110
-          || op == 6'b000111
+      if (op == ADDI
+          || op == SUBI
+          || op == XORI
+          || op == SLLI
+          || op == SRAI
 
-          || op == 6'b010010 // misc
-          || op == 6'b010011
-          || op == 6'b010110
-          || op == 6'b010111
+          || op == FMVLO // misc
+          || op == FMVHI
+          || op == IMOVF
+          || op == FMOVI
 
-          || op == 6'b101000 // load
-          || op == 6'b101100
-          || op == 6'b101010
-          || op == 6'b101110
+          || op == LDI // load
+          || op == FLDI
 
-          || op == 6'b111000 // jump, call
-          || op == 6'b111001
-          || op == 6'b111010
-          || op == 6'b111011
-          || op == 6'b111100
-          || op == 6'b111101
-          || op == 6'b111111)
+          || op == J // jump, call
+          || op == JR
+          || op == CALL
+          || op == CALLR
+          || op == RETURN
+          || op == INPUTB
+          || op == HALT)
         use_rt <= 1'b0;
       else
         use_rt <= 1'b1;
