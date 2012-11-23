d7cd11c7c631b62b7d8dfd385b02a16b5fc6814c
tomykaira <tomykaira@gmail.com>
2012-11-24 01:18:18 +0900

    画面サイズを定数化
    プレゼン用に画面サイズを変更するかもしれない

--- compiler_set/presen_soft/main.ml
+++ compiler_set/presen_soft/main.ml
@@ -9,16 +9,17 @@
 (*NOMINCAML let rec readkbd _ = *)
 (*NOMINCAML   print_newline () in *)
 
+let column_max = 80 in
+let row_max = 30 in
+
 
 (* 1行読み取り配列に格納し,行番号、列番号、長さを返す.
    先頭の空白類文字は読み飛ばし *)
 let rec read_line_sub input n state =
   let c = input_char () in
-  if c = 10 then (input.(n) <- 0; n)
-  else if n >= 80 then (input.(n) <- 0; n)
+  if c = 10 or n >= column_max then (input.(n) <- 0; n)
   else if state = 0 then
-    if c < 33 then read_line_sub input n 0
-    else if c > 126 then read_line_sub input n 0
+    if c < 33 or c > 126 then read_line_sub input n 0
     else (input.(n) <- c; read_line_sub input (n+1) 1)
   else (input.(n) <- c; read_line_sub input (n+1) 1) in
 let rec read_line input  =
@@ -41,7 +42,7 @@ let rec print_line str y x n =
   print_line_sub str y x n 0 in
 
 
-let str = Array.create 80 0 in
+let str = Array.create column_max 0 in
 (* プログラムのメインループ.-1でページ切り替え,-2で終了 *)
 let rec main _ =
   let (y, x, len) = read_line str in
