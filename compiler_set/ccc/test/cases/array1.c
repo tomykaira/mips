fb2eb23622b050705ef19f2016da4e510d51d8db
tomykaira <tomykaira@gmail.com>
2012-12-02 09:35:22 +0900

    array1 の動作をシミュレータで確認

--- compiler_set/ccc/test/cases/array1.c
+++ compiler_set/ccc/test/cases/array1.c
@@ -1,3 +1,20 @@
+/*
+f:
+	addi	$r3, $r3, 10
+	slli	$r3, $r3, 2
+	return
+g:
+	addi	$r4, $r0, 10
+	addi	$r5, $r0, 0
+g_loop:
+	addi	$r5, $r5, 1
+	ldr		$r6, $r3, $r5
+	outputb	$r6
+	blt		$r5, $r4, g_loop
+	nop
+	nop
+	return
+ */
 int x[10];
 
 int f(int t);
