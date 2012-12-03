5d9750add116c21755f6b056537fd76fae85ea9e
tomykaira <tomykaira@gmail.com>
2012-12-03 16:41:58 +0900

    fib に問題がある

--- compiler_set/ccc/test/cases/fib.c
+++ compiler_set/ccc/test/cases/fib.c
@@ -5,12 +5,14 @@ int read_int();
 void print_int(int n);
 
 int fib (int n) {
+  if (n == 0) return 1;
+  if (n == 1) return 1;
   return fib(n - 1) + fib(n - 2);
 }
 
-int main(int argc) {
+int main() {
   int n = 0;
-  n = 10;
+  n = 2;
   n = fib(n);
   print_int(n);
 
