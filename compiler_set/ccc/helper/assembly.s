c8009303282ea2379717453b2b9de4e1ad823435
tomykaira <tomykaira@gmail.com>
2012-12-06 00:50:10 +0900

    Add load_program() assembly function

--- compiler_set/ccc/helper/assembly.s
+++ compiler_set/ccc/helper/assembly.s
@@ -1403,3 +1403,60 @@ print_char:
 	addi	$r3, $r0, 10
 	outputb	$r3
 	return
+
+# from bootloader.s
+
+# r3: temporary
+# r4: loaded instruction
+# r5: opcode (+ 2bits)
+# r8: 0xffffffff
+# r9: pc offset (end of loader)
+# r10: instruction pointer (increment)
+# r11: jump opcode
+# r12: call opcode
+# r13: setl opcode
+load_program:
+	subi	$r8, $r0, 1	# end marker ffffffff
+	setl	$r9, program_end	# offset
+	setl	$r10, program_end	# instruction pointer
+	addi	$r11, $r0, 224	# jump
+	addi	$r12, $r0, 232	# call
+	addi	$r13, $r0, 64	# setl
+load_start:
+	inputb	$r3
+	addi	$r5, $r3, 0	# backup
+	slli	$r4, $r3, 8
+	inputb	$r3
+	add	$r4, $r4, $r3
+	slli	$r4, $r4, 8
+	inputb	$r3
+	add	$r4, $r4, $r3
+	slli	$r4, $r4, 8
+	inputb	$r3
+	add	$r4, $r4, $r3
+	beq	$r4, $r8, load_end
+	nop
+	nop
+
+	# add pc for jump and call
+	beq	$r5, $r11, add_pc
+	nop
+	nop
+	beq	$r5, $r12, add_pc
+	nop
+	nop
+	beq	$r5, $r13, add_pc
+	nop
+	nop
+
+	program	$r10, $r4
+	addi	$r10, $r10, 1
+	j	load_start
+add_pc:
+	add	$r4, $r4, $r9	# add offset
+	program	$r10, $r4
+	addi	$r10, $r10, 1
+	j	load_start
+load_end:
+	call	program_end
+	return
