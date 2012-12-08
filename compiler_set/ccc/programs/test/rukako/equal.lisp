b9409b0967f90f3f31ee50d2a7d27a559339a6da
tomykaira <tomykaira@gmail.com>
2012-12-09 01:11:56 +0900

    equal のケースを複雑にする

--- compiler_set/ccc/programs/test/rukako/equal.lisp
+++ compiler_set/ccc/programs/test/rukako/equal.lisp
@@ -1,2 +1,2 @@
-(print (apply (label equal (lambda (x y) (cond ((atom x) (cond ((atom y) (eq x y)) (t nil))) ((equal (car x) (car y)) (equal (cdr x) (cdr y))) (t nil)))) (quote (1 2 3)) (quote (1 2 3))))
+(print (apply (label equal (lambda (x y) (cond ((atom x) (cond ((atom y) (eq x y)) (t nil))) ((equal (car x) (car y)) (equal (cdr x) (cdr y))) (t nil)))) (quote (1 (2 . 5) 3 (4 . 9) 8)) (quote (1 (2 . 5) 3 (4 . 9) 8))))
 ;;OUTPUT:t
