eb03a83aa1c4bb05e6e99ee853a4273f36df1d15
tomykaira <tomykaira@gmail.com>
2012-12-15 17:10:20 +0900

    inst からデータが流れでて、次の命令が EX にとどくまで sd_read, sd_write を止める

--- 2nd/sdcard/sd_manager.v
+++ 2nd/sdcard/sd_manager.v
@@ -21,10 +21,21 @@ module sd_manager(input clk,
 
    wire [5:0] op;
 
+   reg [1:0] was_ready;
+   wire ready;
+
+   // wait for register delay, this should work like multi-clock cycle
+   always @ (posedge(clk)) begin
+      was_ready[1] <= was_ready[0];
+      was_ready[0] <= sd_ready;
+   end
+
+   assign ready = (sd_ready == 1 || was_ready[0] == 1 || was_ready[1] == 1) ? 1'b1 : 1'b0;
+
    assign op      = inst[31:26];
 
-   assign sd_read  = op == READSD  && sd_ready == 1'b0 ? 1'b1 : 1'b0;
-   assign sd_write = op == WRITESD && sd_ready == 1'b0 ? 1'b1 : 1'b0;
+   assign sd_read  = op == READSD  && ready == 0 ? 1'b1 : 1'b0;
+   assign sd_write = op == WRITESD && ready == 0 ? 1'b1 : 1'b0;
 
    assign sd_write_data = rt[7:0];
    assign sd_addr = rs;
