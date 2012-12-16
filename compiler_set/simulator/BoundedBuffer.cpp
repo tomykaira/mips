1e8594a6dad535b665e4a102c0227b17135f1f6a
tomykaira <tomykaira@gmail.com>
2012-12-16 20:39:07 +0900

    bad_malloc バグを修正
    bad_malloc の原因はジャンプのたびに push される jump_logger で、単純にメモリをくいつぶしていた可能性が高い。
    後から見る上限回数は決まっているので、bounded buffer を導入した。

---
+++ compiler_set/simulator/BoundedBuffer.cpp
@@ -0,0 +1,65 @@
+/**
+ * BoundedBuffer.cpp
+ *
+ */
+#include "BoundedBuffer.hpp"
+#include <ostream>
+#include <iostream>
+
+#include <vector>
+
+using namespace std;
+
+
+/**
+ * Default constructor
+ */
+BoundedBuffer::BoundedBuffer(size_t size) {
+	_content = vector<int>(size);
+	_size = size;
+	_pointer = 0;
+	_valid_element_count = 0;
+}
+
+/**
+ * Default destructor
+ */
+BoundedBuffer::~BoundedBuffer() {
+}
+
+/**
+ * Add an element
+ */
+void BoundedBuffer::add(int element) {
+	cout << "append" << element << endl;
+	_content[_pointer] = element;
+	_pointer = (_pointer + 1) % _size;
+	_valid_element_count = max(_valid_element_count, _pointer);
+}
+
+/**
+ * Get valid elements.  Ordered from old to new.
+ */
+vector<int> BoundedBuffer::valid_elements() const {
+	vector<int> ret;
+	for (int i = 0; i < _valid_element_count; i++) {
+		ret.push_back(_content[(i + _pointer - _valid_element_count + _size) % _size]);
+	}
+	return ret;
+}
+
+/**
+ * stream output operator
+ */
+std::ostream& operator<<(std::ostream& lhs, const BoundedBuffer& rhs) {
+	vector<int> elements = rhs.valid_elements();
+	vector<int>::iterator it = elements.begin();
+	lhs << "BoundedBuffer{";
+	while (it < elements.end()-1) {
+		lhs << (*it) << ", ";
+		it++;
+	}
+	lhs << (*it);
+	lhs << "}";
+	return lhs;
+}
